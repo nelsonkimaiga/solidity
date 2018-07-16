f7f03470080f6990a7caf9fe02ceace2a3ad3770
Erik Kundt <bitshift@posteo.org>
2018-07-16 17:24:07 +0200

    Removes obsolete syntax tests.

--- test/libsolidity/syntaxTests/visibility/interface/function_default050.sol
+++
@@ -1,7 +0,0 @@
-pragma experimental "v0.5.0";
-interface I {
-	function f();
-}
-// ----
-// SyntaxError: (45-58): No visibility specified. Did you intend to add "external"?
-// TypeError: (45-58): Functions in interfaces must be declared external.
